332da595bc8b2f32af01d0e05d3eba4ef26c985c
vamshimankala <mankalavamshi99@outlook.com>
2022-06-27 09:18:44 +0000

    congrats-card-React

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  // Write your code here.
+  <div className="congrats-card-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="profile-image"
+      />
+      <h1 className="card-title">rishi</h1>
+      <p className="card-description">Osmania University</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="watch-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
